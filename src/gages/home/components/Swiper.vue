24b60d7d9d514f268748b1d55dc517a22e6eb7eb
kehanbo <875225214@qq.com>
2020-07-26 14:29:09 +0800

    error

--- src/gages/home/components/Swiper.vue
+++ src/gages/home/components/Swiper.vue
@@ -1,24 +1,36 @@
 <template>
- <swiper :options="swiperOption" :not-next-tick="notNextTick" ref="mySwiper">
+<div class="wrapper">
+ <swiper :options="swiperOption">
     <!-- slides -->
-   <swiper-slide>I'm Slide 1</swiper-slide>
-    <swiper-slide>I'm Slide 2</swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-    <swiper-slide>I'm Slide 5</swiper-slide>
-    <swiper-slide>I'm Slide 6</swiper-slide>
-    <swiper-slide>I'm Slide 7</swiper-slide>
+    <swiper-slide>
+      <img src="http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+       <img src="http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg" alt="">
+    </swiper-slide>
     <!-- Optional controls -->
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
+</div>
 </template>
 
 <script>
 export default {
-    name:"HomeSwiper"
+    name:"HomeSwiper",
+    data () {
+      return {
+        swiperOption : {}
+      }
+    }
 }
 </script>
 
 <style lang='stylus' scoped>
-
+  .wrapper
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
 </style>
--- src/main.js
+++ src/main.js
@@ -6,16 +6,13 @@ import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
-import './assets/styles/border.css' //这个是1像素边框的解决方案
+import './assets/styles/border.css' 
 import fastClick from 'fastclick'
-
-//移动端点击事件延迟300ms解决方案
-//在项目根目录下打开cmd，安装fastclick插件,然后再引用进来
-
 Vue.config.productionTip = false
 //使用fastclick如下，将该方法绑定在body上
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
+console.log(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
