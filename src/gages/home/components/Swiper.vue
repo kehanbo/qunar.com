8dc17aef2e2ab6bece323cf731c38998e7659cc9
kehanbo <875225214@qq.com>
2020-07-26 13:28:05 +0800

    swiper

---
+++ src/gages/home/components/Swiper.vue
@@ -0,0 +1,24 @@
+<template>
+ <swiper :options="swiperOption" :not-next-tick="notNextTick" ref="mySwiper">
+    <!-- slides -->
+   <swiper-slide>I'm Slide 1</swiper-slide>
+    <swiper-slide>I'm Slide 2</swiper-slide>
+    <swiper-slide>I'm Slide 3</swiper-slide>
+    <swiper-slide>I'm Slide 4</swiper-slide>
+    <swiper-slide>I'm Slide 5</swiper-slide>
+    <swiper-slide>I'm Slide 6</swiper-slide>
+    <swiper-slide>I'm Slide 7</swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+    name:"HomeSwiper"
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,18 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
-//这个是1像素边框的解决方案
-import './assets/styles/border.css'
-//移动端点击事件延迟300ms解决方案
-//在项目根目录下打开cmd，安装fastclick插件,然后再引用进来
+import './assets/styles/border.css' //这个是1像素边框的解决方案
 import fastClick from 'fastclick'
 
+//移动端点击事件延迟300ms解决方案
+//在项目根目录下打开cmd，安装fastclick插件,然后再引用进来
 
 Vue.config.productionTip = false
 //使用fastclick如下，将该方法绑定在body上
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
